ac3f7a5bbe83ad66128f86f394ac1c5a1b36ddb5
Ananya Pundir <ananyapundir261@gmail.com>
2025-03-04 22:25:48 +0530

    Image Uploader

---
+++ frontend/src/app/imageuploader/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import ImgUploader from "@/components/imageuploader/imgUploader";
+
+function page() {
+  return (
+    <div>
+      <ImgUploader/>
+    </div>
+    
+  )
+}
+
+export default page
