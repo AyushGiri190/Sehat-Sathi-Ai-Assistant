b0e912e7c95f1b23ccccad38047866ba11c77bfc
Ananya Pundir <ananyapundir261@gmail.com>
2025-03-07 01:05:26 +0530

    adding scrollbar and add responsiveness to homepage

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,9 +1,11 @@
 import ContactPage from "@/components/contact/contact"
+import ContactUs from "@/components/navBar/contactUs"
 
 function page() {
     return (
       <div>
             <ContactPage/>
+            
       </div>
       
     )
