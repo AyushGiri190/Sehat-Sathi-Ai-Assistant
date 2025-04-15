f609ecfe786c9c9c91b5d495e094c976541872e9
Abhi Rawat <rawatab.hii23@fmail.com>
2025-04-16 03:41:50 +0530

    breaad-crumb

--- frontend/src/app/imageuploader/skinup/page.tsx
+++ frontend/src/app/imageuploader/skinup/page.tsx
@@ -2,6 +2,7 @@ import ScrollBar from "@/components/contact/scrollbar"
 import SkinImgUp from "@/components/imageuploader/skinImgUp"
 import Navbar from "@/components/navBar/navBar"
 import SkinCancerPage from "@/components/page/knoSkin"
+import BreadcrumbWithCustomSeparator from "@/components/pageBreadcrumb/pgBreadcrumb";
 
 function page() {
   return (
@@ -9,6 +10,9 @@ function page() {
       <Navbar/>
       <ScrollBar/>
       <div className="custom-scrollbar h-screen overflow-y-auto">
+      <div className="ml-6 mt-2">
+      <BreadcrumbWithCustomSeparator currentPage="Skin Cancer"/>
+      </div>
       <SkinCancerPage/>
       <SkinImgUp/>
       </div>
--- frontend/src/app/imageuploader/tuberup/page.tsx
+++ frontend/src/app/imageuploader/tuberup/page.tsx
@@ -2,6 +2,7 @@ import ScrollBar from "@/components/contact/scrollbar";
 import TuberImgUp from "@/components/imageuploader/tuberImgUp";
 import Navbar from "@/components/navBar/navBar";
 import TuberPage from "@/components/page/knoTuber";
+import BreadcrumbWithCustomSeparator from "@/components/pageBreadcrumb/pgBreadcrumb";
 
 function page() {
   return (
@@ -9,6 +10,9 @@ function page() {
       <Navbar/>
       <ScrollBar/>
       <div className="custom-scrollbar h-screen overflow-y-auto">
+      <div className="ml-6 mt-2">
+      <BreadcrumbWithCustomSeparator currentPage="Tuberculosis"/>
+      </div>
       <TuberPage/>
       <TuberImgUp/>
       </div>
